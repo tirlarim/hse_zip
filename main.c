e44c32aecd595381c96b5f27c0088a4b4b3cbfaa
StasRomanov <stasromanov801@gmail.com>
2022-03-01 20:30:22 +0300

    read as bin fix

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main() {
   long int size = ftell(fp); // now you got size of file in bytes
   fseek(fp, 0, SEEK_SET);    // same as rewind(fp)
   for (int i = 0; i < size; i++) {
-    fread(&a,sizeof(int),1,fp); // you read one int (sizeof(int)!=1 byte)
+    fread(&a,sizeof(int)/4,1,fp); // you read one int (sizeof(int)!=1 byte)
     if (i % 5 == 0 && i != 0) {
       printf("\n");
     }
-    printf("0x%x\t", a);
+    printf("0x%x -> %c\t", a, a);
   }
   printf("\n");
   printf("Size of file: %ld bytes.", size);
