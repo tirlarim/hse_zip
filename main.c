979364b74f291d30e39da14d76692c62b50cb215
StasRomanov <stasromanov801@gmail.com>
2022-03-01 20:18:47 +0300

    read as bin

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!\n");
+  FILE *fp;
+  int a = 0;
+  fp = fopen("../testData/example.txt","rb");
+  fseek(fp, 0, SEEK_END);
+  long int size = ftell(fp); // now you got size of file in bytes
+  fseek(fp, 0, SEEK_SET);    // same as rewind(fp)
+  for (int i = 0; i < size; i++) {
+    fread(&a,sizeof(int),1,fp); // you read one int (sizeof(int)!=1 byte)
+    if (i % 5 == 0 && i != 0) {
+      printf("\n");
+    }
+    printf("0x%x\t", a);
+  }
+  printf("\n");
+  printf("Size of file: %ld bytes.", size);
+  fclose(fp);
   return 0;
 }
