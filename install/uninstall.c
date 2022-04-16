638ebed00966c2b09d6591ccdfcfbde17224b444
StasRomanov <47898034+StasRomanov@users.noreply.github.com>
2022-04-16 13:19:33 +0300

    Update uninstall.c

--- install/uninstall.c
+++ install/uninstall.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
 #include <direct.h>
-
-#ifdef __cplusplus__
-  #include <cstdlib>
-#else
-  #include <stdlib.h>
-#endif
+#include <stdlib.h>
 
 void main(){
 	char dir[FILENAME_MAX];
 	_getcwd(dir, sizeof(dir));
 	FILE* f = fopen("unins.reg","w");
-	
-	char def[] = "Windows Registry Editor Version 5.00\n[-HKEY_CLASSES_ROOT\\*\\shell\\Compress]";
-	
-	fprintf(f,"%s",def);
-
-	
-	char def1[] = "Windows Registry Editor Version 5.00\n[-HKEY_CLASSES_ROOT\\.hse_zip]";
-	fprintf(f, "%s", def1);
-
+	char def1[] = "Windows Registry Editor Version 5.00\n[-HKEY_CLASSES_ROOT\\*\\shell\\Compress]";
+	char def2[] = "Windows Registry Editor Version 5.00\n[-HKEY_CLASSES_ROOT\\.hse_zip]";
+	fprintf(f,"%s",def1);
+	fprintf(f, "%s", def2);
 	fclose(f);
-
 	system("Regedit unins.reg");
 }
